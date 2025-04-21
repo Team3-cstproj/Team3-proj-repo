c1f4b792189071d28dba9bec04ea4c84531cebc4
Abdelkader15 <https://github.com/Abdelkader15>
2025-04-21 22:04:12 +0200

    load nav bar in html css js product file

---
+++ js/product.js
@@ -0,0 +1,45 @@
+//nav bar -----strart
+//  cart list baby
+const cartBtnList = document.querySelectorAll(".cart-trigger");
+const cartSidebar = document.getElementById("cartSidebar");
+const cartOverlay = document.getElementById("cartOverlay");
+const closeCartBtn = document.getElementById("closeCart");
+
+cartBtnList.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    cartSidebar.classList.add("active");
+    cartOverlay.classList.add("active");
+  });
+});
+
+closeCartBtn.addEventListener("click", () => {
+  cartSidebar.classList.remove("active");
+  cartOverlay.classList.remove("active");
+});
+
+cartOverlay.addEventListener("click", () => {
+  cartSidebar.classList.remove("active");
+  cartOverlay.classList.remove("active");
+});
+////btns color baby
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll(".offer-banner .btn").forEach((button) => {
+    function activate() {
+      button.style.backgroundColor = "white";
+      button.style.color = "black";
+    }
+
+    function deactivate() {
+      button.style.backgroundColor = "transparent";
+      button.style.color = "white";
+    }
+
+    button.addEventListener("mousedown", activate);
+    button.addEventListener("mouseup", deactivate);
+    button.addEventListener("mouseleave", deactivate);
+    button.addEventListener("touchstart", activate);
+    button.addEventListener("touchend", deactivate);
+  });
+});
+// nav bar -------end
