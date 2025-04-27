1febb28102b1c0856ef7f86735bc96ef9036d82c
ziad <ziad@gmail.com>
2025-04-27 23:36:14 +0300

    seller

--- js/admin(seller).js
+++ js/admin(seller).js
@@ -7,3 +7,33 @@ toggleBtn.addEventListener('click', function () {
   sidebar.classList.toggle('collapsed');
   mainContent.classList.toggle('collapsed');
 });
+
+// load localstorage in table
+function loadProducts() {
+  const sellers = JSON.parse(localStorage.getItem('sellers')) || [];
+  const tableBody = document.querySelector("tbody");
+  tableBody.innerHTML = ''; 
+
+  sellers.forEach((seller) => {
+    const row = `
+      <tr>
+                  <td>${seller.id}</td>
+                  <td>${seller.username}</td>
+                  <td>${seller.email}</td>
+                  <td>
+                    <div class="remove-icon" onclick="removeProduct('${seller.id}')">
+                      <i class="fa-solid fa-xmark"></i>
+                    </div>
+                  </td>
+                </tr>
+    `;
+    tableBody.innerHTML += row;
+  });
+}
+function removeProduct(productId) {
+  const sellers = JSON.parse(localStorage.getItem('sellers')) || [];
+  const updatedProducts = sellers.filter(seller => String(seller.id) !== productId);
+  localStorage.setItem('sellers', JSON.stringify(updatedProducts)); 
+  loadProducts(); 
+}
+loadProducts();
